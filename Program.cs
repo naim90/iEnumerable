3782d78bb96f8fa06289ab0e6189c3e43d0586ad
naim90 <naim.elmoudni@gmail.com>
2021-10-03 16:21:39 +0200

    Calcul moyenne éléments inférieur ou égale à n

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerable
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Entrez la valeur de n :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            List<int> tableauInt =
+                new List<int> {1,5,0,8,28,53,76,10,32,4,6,0};
+
+            IEnumerable<int> parcoursChaquesValeurs = tableauInt.Where(x => x <= n);
+            double moyenne = parcoursChaquesValeurs.Average();
+
+            Console.WriteLine("Tableau des valeurs inférieur ou égal à "+n);
+
+
+            foreach (int x in parcoursChaquesValeurs)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("La moyenne des éléments inférieur ou égal à "+n+" est : "+average);
+
+        }
+    }
+}
